7054fb6014ef9dd5ade493cd126556cd8ebe3ec1
sunder-xie <xieshengrong@live.com>
2017-10-31 14:50:18 +0800

    预警系统基础架构 v1.0.2

--- panoramic-monitor-risk-warning/src/main/java/com/risk/warning/RiskWarningApplication.java
+++ panoramic-monitor-risk-warning/src/main/java/com/risk/warning/RiskWarningApplication.java
@@ -1,22 +1,17 @@
 package com.risk.warning;
 
-/**
- * @author sunder
- * 预警系统
- */
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
+ * @author sunder
  * 启动类
+ * 在classpath 下 两种格式是相同的
+ * @EnableEurekaClient 更依赖于eureka注册中心
+ * @EnableDiscoveryClient 可以在eureka中心或者zk，consul.所以更通用
  */
 @SpringBootApplication
-/** 在classpath 下 两种格式是相同的
- *  @EnableEurekaClient 更依赖于eureka注册中心
- *  @EnableDiscoveryClient 可以在eureka中心或者zk，consul.所以更通用
- *  */
 @EnableDiscoveryClient
 public class RiskWarningApplication {
     public static void main(String[] args) {
