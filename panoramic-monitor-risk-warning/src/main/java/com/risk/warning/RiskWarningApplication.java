518755111cb6861524e4336820f8a58282e08313
sunder-xie <xieshengrong@live.com>
2017-10-31 11:25:13 +0800

    后台系统v1.0

--- panoramic-monitor-risk-warning/src/main/java/com/risk/warning/RiskWarningApplication.java
+++ panoramic-monitor-risk-warning/src/main/java/com/risk/warning/RiskWarningApplication.java
@@ -4,5 +4,22 @@ package com.risk.warning;
  * @author sunder
  * 预警系统
  */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 启动类
+ */
+@SpringBootApplication
+/** 在classpath 下 两种格式是相同的
+ *  @EnableEurekaClient 更依赖于eureka注册中心
+ *  @EnableDiscoveryClient 可以在eureka中心或者zk，consul.所以更通用
+ *  */
+@EnableDiscoveryClient
 public class RiskWarningApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RiskWarningApplication.class, args);
+    }
 }
