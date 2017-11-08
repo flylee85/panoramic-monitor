77ed215b8fa1bc1a959064acf705da10ed9c2522
sunder-xie <xieshengrong@live.com>
2017-11-08 11:38:41 +0800

    添加扫包

--- panoramic-monitor-discovery-eureka/src/main/java/com/cloud/EurekaApplication.java
+++ panoramic-monitor-discovery-eureka/src/main/java/com/cloud/EurekaApplication.java
@@ -3,12 +3,14 @@ package com.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 使用Eureka做服务发现。
  */
 @SpringBootApplication
 @EnableEurekaServer
+@ComponentScan(basePackages = {"com.cloud", "com"})
 public class EurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
--- panoramic-monitor-manage-provider/src/main/java/com/monitor/manage/ManageApplication.java
+++ panoramic-monitor-manage-provider/src/main/java/com/monitor/manage/ManageApplication.java
@@ -3,16 +3,15 @@ package com.monitor.manage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author sunder
  * 启动类
- * 在classpath 下 两种格式是相同的
- * @EnableEurekaClient 更依赖于eureka注册中心
- * @EnableDiscoveryClient 可以在eureka中心或者zk，consul.所以更通用
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = {"com.cloud", "com.monitor", "com"})
 public class ManageApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManageApplication.class, args);
