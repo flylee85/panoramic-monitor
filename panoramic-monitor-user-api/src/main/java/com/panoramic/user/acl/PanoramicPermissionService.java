ca419045ab2b30f168eea1ae6dc4ef682ce33fcb
sunder-xie <xieshengrong@live.com>
2017-12-29 10:23:32 +0800

    1,添加注释，优化代码规范

--- panoramic-monitor-user-api/src/main/java/com/panoramic/user/acl/PanoramicPermissionService.java
+++ panoramic-monitor-user-api/src/main/java/com/panoramic/user/acl/PanoramicPermissionService.java
@@ -1,18 +1,14 @@
 package com.panoramic.user.acl;
 
 
-import org.springframework.transaction.annotation.Transactional;
-
 import com.cloud.core.Service;
-import com.cloud.core.ServiceException;
 import com.monitor.model.permission.PanoramicPermission;
 
 /**
-*@author summer
-* 2017/11/08.
-*/
+ * @author summer
+ * 2017/11/08.
+ */
 
-@Transactional(readOnly = true, rollbackFor = ServiceException.class)
 public interface PanoramicPermissionService extends Service<PanoramicPermission> {
 
 }
--- panoramic-monitor-user-provider/src/main/java/com/cloud/web/controller/role/PanoramicRoleController.java
+++ panoramic-monitor-user-provider/src/main/java/com/cloud/web/controller/role/PanoramicRoleController.java
@@ -1,54 +1,51 @@
 package com.cloud.web.controller.role;
 
-import com.cloud.api.vo.ResultCode;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
-import com.panoramic.user.acl.PanoramicRoleService;
-import com.panoramic.user.entity.PanoramicRole;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.panoramic.user.acl.PanoramicRoleService;
 
 /**
- * @author sunmer
- * on 2017/11/06.
+ * @author sunmer on 2017/11/06.
  */
 @RestController
 @RequestMapping("/role")
 public class PanoramicRoleController {
-    @Autowired
-    private PanoramicRoleService panoramicRoleService;
+	@Autowired
+	@Qualifier("roleService")
+	private PanoramicRoleService roleService;
 
-//    @PostMapping
-//    public ResultCode<PanoramicRole> add(PanoramicRole panoramicRole) {
-//        panoramicRoleService.save(panoramicRole);
-//        return ResultCode.getSuccessReturn(panoramicRole);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResultCode<PanoramicRole> delete(@PathVariable Integer id) {
-//        panoramicRoleService.deleteById(id);
-//        return ResultCode.getSuccessMap();
-//    }
-//
-//    @PutMapping
-//    public ResultCode<PanoramicRole> update(PanoramicRole panoramicRole) {
-//        panoramicRoleService.update(panoramicRole);
-//        return ResultCode.getSuccessReturn(panoramicRole);
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResultCode<PanoramicRole> detail(@PathVariable Integer id) {
-//        PanoramicRole panoramicRole = panoramicRoleService.findById(id);
-//        return ResultCode.getSuccessReturn(panoramicRole);
-//    }
-//
-//    @GetMapping
-//    public ResultCode<PageInfo> list(Integer page, Integer size) {
-//        PageHelper.startPage(page, size);
-//        List<PanoramicRole> list = panoramicRoleService.findAll();
-//        PageInfo pageInfo = new PageInfo(list);
-//        return ResultCode.getSuccessReturn(pageInfo);
-//    }
+	// @PostMapping
+	// public ResultCode<PanoramicRole> add(PanoramicRole panoramicRole) {
+	// roleService.save(panoramicRole);
+	// return ResultCode.getSuccessReturn(panoramicRole);
+	// }
+	//
+	// @DeleteMapping("/{id}")
+	// public ResultCode<PanoramicRole> delete(@PathVariable Integer id) {
+	// roleService.deleteById(id);
+	// return ResultCode.getSuccessMap();
+	// }
+	//
+	// @PutMapping
+	// public ResultCode<PanoramicRole> update(PanoramicRole panoramicRole) {
+	// roleService.update(panoramicRole);
+	// return ResultCode.getSuccessReturn(panoramicRole);
+	// }
+	//
+	// @GetMapping("/{id}")
+	// public ResultCode<PanoramicRole> detail(@PathVariable Integer id) {
+	// PanoramicRole panoramicRole = roleService.findById(id);
+	// return ResultCode.getSuccessReturn(panoramicRole);
+	// }
+	//
+	// @GetMapping
+	// public ResultCode<PageInfo> list(Integer page, Integer size) {
+	// PageHelper.startPage(page, size);
+	// List<PanoramicRole> list = roleService.findAll();
+	// PageInfo pageInfo = new PageInfo(list);
+	// return ResultCode.getSuccessReturn(pageInfo);
+	// }
 }
