2060d110a676a5b78783ac68934644f34885be49
sunder-xie <xieshengrong@live.com>
2018-01-04 13:56:59 +0800

    1,添加注释，优化代码规范 2,添加发布war支持插件

--- panoramic-monitor-discovery-eureka/src/main/java/com/cloud/EurekaApplication.java
+++ panoramic-monitor-discovery-eureka/src/main/java/com/cloud/EurekaApplication.java
@@ -1,27 +1,33 @@
 package com.cloud;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.ComponentScan;
-
 import com.cloud.util.LoggerUtils;
 import com.cloud.util.TLogger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * 使用Eureka做服务发现。
+ *
  * @author summer
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
-	private static final transient TLogger DB_LOGGER = LoggerUtils.getLogger(EurekaApplication.class);
+public class EurekaApplication extends SpringBootServletInitializer {
+    private static final transient TLogger DB_LOGGER = LoggerUtils.getLogger(EurekaApplication.class);
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
-		DB_LOGGER.warn("EurekaApplication started successfully");
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+        DB_LOGGER.warn("EurekaApplication started successfully");
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EurekaApplication.class);
+    }
 }
