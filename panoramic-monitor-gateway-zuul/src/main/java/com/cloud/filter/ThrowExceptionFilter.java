4309165448b335e32c911bed0f391ddb9e0237a0
sunder-xie <xieshengrong@live.com>
2018-01-05 15:12:37 +0800

    1,添加注释，优化代码规范 2,优化添加统一异常过滤器

---
+++ panoramic-monitor-gateway-zuul/src/main/java/com/cloud/filter/ThrowExceptionFilter.java
@@ -0,0 +1,50 @@
+package com.cloud.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author summer  网关统一异常过滤器
+ */
+public class ThrowExceptionFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(ThrowExceptionFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        return ctx.containsKey("error.status_code") && !ctx.getBoolean("error.status_code", false);
+    }
+
+    @Override
+    public Object run() {
+        log.info("This is a pre filter, it will throw a RuntimeException");
+        RequestContext ctx = RequestContext.getCurrentContext();
+        try {
+            doSomething();
+        } catch (Exception e) {
+            ctx.set("error.status_code", HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            ctx.set("error.exception", e);
+        }
+        return null;
+    }
+
+    private void doSomething() {
+        throw new RuntimeException("Exist some errors...");
+    }
+
+}
