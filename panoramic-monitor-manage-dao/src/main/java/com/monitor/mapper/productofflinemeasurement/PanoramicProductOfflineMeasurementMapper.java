15df2081108719c0720b8d07161283ace56778b1
gang <xuegang99@gmail.com>
2018-01-08 18:23:26 +0800

    1.产品下线实时任务追加

--- panoramic-monitor-manage-dao/src/main/java/com/monitor/mapper/productofflinemeasurement/PanoramicProductOfflineMeasurementMapper.java
+++ panoramic-monitor-manage-dao/src/main/java/com/monitor/mapper/productofflinemeasurement/PanoramicProductOfflineMeasurementMapper.java
@@ -63,5 +63,20 @@ public interface PanoramicProductOfflineMeasurementMapper extends Mapper<Panoram
     		@Param("code") String code,
     		@Param("date") String date,
     		@Param("startHour") String startHour,
-    		@Param("endHour") String endHour);    
+    		@Param("endHour") String endHour);
+    
+    /**
+     * 查询当前产品下线
+     * @return
+     */
+    @Select("SELECT\n" + 
+    		"	distinct code,name\n" + 
+    		"FROM\n" + 
+    		"	panoramic_product_offline_measurement\n" + 
+    		"WHERE\n" + 
+    		"	in_or_out = 0\n" + 
+    		"AND delete_flag = 1\n" + 
+    		"AND f_id = 2\n" + 
+    		"")
+    List<PanoramicProductOfflineMeasurement> listProductOfflineCategory();
 }
