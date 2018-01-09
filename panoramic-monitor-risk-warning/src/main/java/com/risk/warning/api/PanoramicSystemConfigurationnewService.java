6ed562bc0207a52f63f1eca947534c2f0b120cab
陈伟林 <slayersfox@vip.qq.com>
2018-01-09 13:48:49 +0800

    预警模块根据规则自动生成SQL文接口

---
+++ panoramic-monitor-risk-warning/src/main/java/com/risk/warning/api/PanoramicSystemConfigurationnewService.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+/**
+ * @author fgh
+ *
+ */
+package com.risk.warning.api;
+
+
+import com.cloud.core.Service;
+import com.risk.warning.model.PanoramicSystemConfigurationnew;
+
+import java.util.List;
+
+
+/**
+ * @author 
+ */
+
+public interface PanoramicSystemConfigurationnewService extends Service<PanoramicSystemConfigurationnew> {
+
+    /**
+     * 生成SQL文
+     */
+    void createSqlService();
+    
+
+}
