3ed6222a39716299eabb2bd4fab79402095b7a79
sunder-xie <xieshengrong@live.com>
2018-01-11 17:43:23 +0800

    1,添加注释，优化代码规范 2,修复首页物资监管库存数量问题

---
+++ panoramic-monitor-manage-provider/src/main/java/com/monitor/constant/DailyInventorySummaryCodeConstant.java
@@ -0,0 +1,28 @@
+package com.monitor.constant;
+
+import org.apache.commons.collections.list.UnmodifiableList;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 首页查询code
+ *
+ * @author summer
+ */
+public class DailyInventorySummaryCodeConstant {
+    public static final List<String> temp = Arrays.asList(
+            "HG01XY750300",
+            "HG01XY750200",
+            "HG01XY750100",
+            "HG01XY750000",
+            "HG01XY750510",
+            "HG01XY750410"
+    );
+    public static final List<String> codeList;
+
+    static {
+        codeList = UnmodifiableList.decorate(temp);
+    }
+
+}
