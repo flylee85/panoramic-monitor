f27d43698bdf301b8f1f99443b5678d8fa711f8a
陈伟林 <slayersfox@vip.qq.com>
2018-01-12 18:03:41 +0800

    版本回滚

--- panoramic-monitor-risk-warning/src/main/java/com/risk/warning/web/controller/test/TestController.java
+++ panoramic-monitor-risk-warning/src/main/java/com/risk/warning/web/controller/test/TestController.java
@@ -1,27 +1,8 @@
 package com.risk.warning.web.controller.test;
 
-import com.alibaba.fastjson.JSONArray;
 import com.cloud.api.vo.ResultCode;
-import com.cloud.util.HttpUtils;
-
-import antlr.collections.List;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLDecoder;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,134 +20,7 @@ public class TestController {
 
     @ApiOperation(value = "测试接口", notes = "测试")
     @GetMapping("/{id}")
-    public ResultCode<Void> test(@PathVariable Integer id)  {
-        //发送 GET 请求
-    	String Url = "http://g7s.api.huoyunren.com/interface/index.php";
-    	String method = "order.device.search";
-    	String app_key = "xzdsj_api";
-        String data = "{\"devtype\":\"1\"}";
-        String app_secret = "c7618ffb01363153d8d1657697d58f8a";
-        Date dt = new Date();
-        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");  
-        String timestamp = sdf.format(dt);  
-        System.out.println("timestamp=" + timestamp );
-        String mae =  app_secret + "app_key" + app_key + "data" + data + "method" + method + "timestamp" + timestamp + app_secret;
-
-        System.out.println("mae=" + mae );
-        String sign = md5Password(mae);
-        System.out.println("sign" + sign );
-        String param =  "method=" + method + "&timestamp=" + timestamp + "&app_key=" + app_key + "&sign=" + sign + "&data=" + data;
-
-        
-        String s=convert(sendPost(Url,param));
-
-
-        System.out.println(s);
-        
+    public ResultCode<Void> test(@PathVariable Integer id) {
         return ResultCode.SUCCESS;
     }
-    
-
-    /**
-     * 向指定 URL 发送POST方法的请求
-     * 
-     * @param url
-     *            发送请求的 URL
-     * @param param
-     *            请求参数，请求参数应该是 name1=value1&name2=value2 的形式。
-     * @return 所代表远程资源的响应结果
-     */
-    public static String sendPost(String url, String param) {
-        PrintWriter out = null;
-        BufferedReader in = null;
-        String result = "";
-        try {
-            URL realUrl = new URL(url);
-            // 打开和URL之间的连接
-            URLConnection conn = realUrl.openConnection();
-            // 设置通用的请求属性
-            conn.setRequestProperty("accept", "*/*");
-            conn.setRequestProperty("connection", "Keep-Alive");
-            conn.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-            // 发送POST请求必须设置如下两行
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            // 获取URLConnection对象对应的输出流
-            out = new PrintWriter(conn.getOutputStream());
-            // 发送请求参数
-            out.print(param);
-            // flush输出流的缓冲
-            out.flush();
-            // 定义BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream()));
-            String line;
-            while ((line = in.readLine()) != null) {
-                result += line;
-            }
-        } catch (Exception e) {
-            System.out.println("发送 POST 请求出现异常！"+e);
-            e.printStackTrace();
-        }
-        //使用finally块来关闭输出流、输入流
-        finally{
-            try{
-                if(out!=null){
-                    out.close();
-                }
-                if(in!=null){
-                    in.close();
-                }
-            }
-            catch(IOException ex){
-                ex.printStackTrace();
-            }
-        }
-        return result;
-    }
-    
-    public static String md5Password(String password) {
-
-        try {
-            // 得到一个信息摘要器
-            MessageDigest digest = MessageDigest.getInstance("md5");
-            byte[] result = digest.digest(password.getBytes());
-            StringBuffer buffer = new StringBuffer();
-            // 把每一个byte 做一个与运算 0xff;
-            for (byte b : result) {
-                // 与运算
-                int number = b & 0xff;// 加盐
-                String str = Integer.toHexString(number);
-                if (str.length() == 1) {
-                    buffer.append("0");
-                }
-                buffer.append(str);
-            }
-
-            // 标准的md5加密后的结果
-            return buffer.toString().toUpperCase();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            return "";
-        }
-    }
-    
-    public String convert(String utfString){
-        StringBuilder sb = new StringBuilder();
-        int i = -1;
-        int pos = 0;
-         
-        while((i=utfString.indexOf("\\u", pos)) != -1){
-            sb.append(utfString.substring(pos, i));
-            if(i+5 < utfString.length()){
-                pos = i+6;
-                sb.append((char)Integer.parseInt(utfString.substring(i+2, i+6), 16));
-            }
-        }
-         
-        return sb.toString();
-    }
-    
-    
 }
