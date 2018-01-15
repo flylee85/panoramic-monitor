70b21040874074be5f7ea037558e5febb74f4947
sunder-xie <xieshengrong@live.com>
2018-01-15 09:11:30 +0800

    1,添加注释，优化代码规范

--- panoramic-monitor-scheduled-task/src/main/java/com/cloud/service/acl/ScheduleTriggerService.java
+++ panoramic-monitor-scheduled-task/src/main/java/com/cloud/service/acl/ScheduleTriggerService.java
@@ -11,7 +11,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -29,9 +28,9 @@ public class ScheduleTriggerService {
      * 获取锁的最大延迟时间（s）
      */
     private static final int DELAY = 10;
+    private static final ReentrantLock LOCK = new ReentrantLock();
     @Autowired
     private Scheduler scheduler;
-
     @Autowired
     @Qualifier("scheduleTriggerMapper")
     private ScheduleTriggerMapper scheduleTriggerMapper;
@@ -41,65 +40,63 @@ public class ScheduleTriggerService {
      */
     @Scheduled(fixedRate = 1000 * 30)
     public void refreshTrigger() {
-        ReentrantLock lock = new ReentrantLock();
+        LOCK.lock();
         try {
-            if (lock.tryLock(DELAY, TimeUnit.SECONDS)) {
-                List<ScheduleTrigger> jobList = scheduleTriggerMapper.selectAll();
-                if (null != jobList && jobList.size() > 0) {
-                    for (ScheduleTrigger scheduleJob : jobList) {
-                        String status = scheduleJob.getStatus();
-                        TriggerKey triggerKey = TriggerKey.triggerKey(scheduleJob.getJobName(), scheduleJob.getJobGroup());
-                        CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);
-                        // 说明本条任务还没有添加到quartz中
-                        if (null == trigger) {
+            List<ScheduleTrigger> jobList = scheduleTriggerMapper.selectAll();
+            if (null != jobList && jobList.size() > 0) {
+                for (ScheduleTrigger scheduleJob : jobList) {
+                    String status = scheduleJob.getStatus();
+                    TriggerKey triggerKey = TriggerKey.triggerKey(scheduleJob.getJobName(), scheduleJob.getJobGroup());
+                    CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);
+                    // 说明本条任务还没有添加到quartz中
+                    if (null == trigger) {
+                        if (STATUS.equals(status)) {
+                            continue;
+                        }
+                        try {
+                            // 创建JobDetail（数据库中job_name存的任务全路径，这里就可以动态的把任务注入到JobDetail中）
+                            JobDetail jobDetail = JobBuilder
+                                    .newJob((Class<? extends Job>) Class.forName(scheduleJob.getJobName()))
+                                    .withIdentity(scheduleJob.getJobName(), scheduleJob.getJobGroup()).build();
+                            CronScheduleBuilder scheduleBuilder = CronScheduleBuilder
+                                    .cronSchedule(scheduleJob.getCron());
+                            trigger = TriggerBuilder.newTrigger()
+                                    .withIdentity(scheduleJob.getJobName(), scheduleJob.getJobGroup())
+                                    .withSchedule(scheduleBuilder).build();
+                            scheduler.scheduleJob(jobDetail, trigger);
+                        } catch (ClassNotFoundException e) {
+                            logger.error("执行任务调度出现异常ClassNotFoundException {}", e);
+                        }
+                    } else {
+                        // 如果是禁用，从quartz中删除这条任务
+                        try {
                             if (STATUS.equals(status)) {
+                                JobKey jobKey = JobKey.jobKey(scheduleJob.getJobName(), scheduleJob.getJobGroup());
+                                scheduler.deleteJob(jobKey);
                                 continue;
                             }
-                            try {
-                                // 创建JobDetail（数据库中job_name存的任务全路径，这里就可以动态的把任务注入到JobDetail中）
-                                JobDetail jobDetail = JobBuilder
-                                        .newJob((Class<? extends Job>) Class.forName(scheduleJob.getJobName()))
-                                        .withIdentity(scheduleJob.getJobName(), scheduleJob.getJobGroup()).build();
-                                CronScheduleBuilder scheduleBuilder = CronScheduleBuilder
-                                        .cronSchedule(scheduleJob.getCron());
-                                trigger = TriggerBuilder.newTrigger()
-                                        .withIdentity(scheduleJob.getJobName(), scheduleJob.getJobGroup())
-                                        .withSchedule(scheduleBuilder).build();
-                                scheduler.scheduleJob(jobDetail, trigger);
-                            } catch (ClassNotFoundException e) {
-                                logger.error("执行任务调度出现异常ClassNotFoundException {}", e);
-                            } finally {
-                                lock.unlock();
+                            String searchCron = scheduleJob.getCron();
+                            String currentCron = trigger.getCronExpression();
+                            if (!searchCron.equals(currentCron)) {
+                                // 表达式调度构建器
+                                CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(searchCron);
+                                // 按新的cronExpression表达式重新构建trigger
+                                trigger = trigger.getTriggerBuilder().withIdentity(triggerKey).withSchedule(scheduleBuilder)
+                                        .build();
+                                // 按新的trigger重新设置job执行
+                                scheduler.rescheduleJob(triggerKey, trigger);
                             }
-                        } else {
-                            // 如果是禁用，从quartz中删除这条任务
-                            try {
-                                if (STATUS.equals(status)) {
-                                    JobKey jobKey = JobKey.jobKey(scheduleJob.getJobName(), scheduleJob.getJobGroup());
-                                    scheduler.deleteJob(jobKey);
-                                    continue;
-                                }
-                                String searchCron = scheduleJob.getCron();
-                                String currentCron = trigger.getCronExpression();
-                                if (!searchCron.equals(currentCron)) {
-                                    // 表达式调度构建器
-                                    CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(searchCron);
-                                    // 按新的cronExpression表达式重新构建trigger
-                                    trigger = trigger.getTriggerBuilder().withIdentity(triggerKey).withSchedule(scheduleBuilder)
-                                            .build();
-                                    // 按新的trigger重新设置job执行
-                                    scheduler.rescheduleJob(triggerKey, trigger);
-                                }
-                            } catch (Exception e) {
-                            } finally {
-                                lock.unlock();
-                            }
+                        } catch (Exception e) {
                         }
                     }
                 }
             }
+
         } catch (Exception e) {
             logger.error("定时任务每日刷新触发器任务异常，在ScheduleTriggerService的方法refreshTrigger中，异常信息：", e);
+        } finally {
+            LOCK.unlock();
         }
+
     }
 }
