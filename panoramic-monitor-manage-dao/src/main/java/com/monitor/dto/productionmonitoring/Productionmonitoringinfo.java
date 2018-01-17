8ad586f035be550a117fde9a807c72a0e4ce7086
sunder-xie <xieshengrong@live.com>
2018-01-17 16:50:40 +0800

    1,添加注释，优化代码规范 2,修复发布Lombok问题

--- panoramic-monitor-manage-dao/src/main/java/com/monitor/dto/productionmonitoring/Productionmonitoringinfo.java
+++ panoramic-monitor-manage-dao/src/main/java/com/monitor/dto/productionmonitoring/Productionmonitoringinfo.java
@@ -14,6 +14,46 @@ public class Productionmonitoringinfo extends BaseObject {
      */
     private double cpoc;
 
+    public double getCpoc() {
+        return cpoc;
+    }
+
+    public void setCpoc(double cpoc) {
+        this.cpoc = cpoc;
+    }
+
+    public double getCpac() {
+        return cpac;
+    }
+
+    public void setCpac(double cpac) {
+        this.cpac = cpac;
+    }
+
+    public double getCcp() {
+        return ccp;
+    }
+
+    public void setCcp(double ccp) {
+        this.ccp = ccp;
+    }
+
+    public double getCpc() {
+        return cpc;
+    }
+
+    public void setCpc(double cpc) {
+        this.cpc = cpc;
+    }
+
+    public double getPcc() {
+        return pcc;
+    }
+
+    public void setPcc(double pcc) {
+        this.pcc = pcc;
+    }
+
     /**
      * 磷钙酸耗
      */
