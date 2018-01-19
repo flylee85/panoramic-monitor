2fe3d708d22f2ab6c56b956c4b9069a652e7d0a6
sunder-xie <xieshengrong@live.com>
2018-01-19 14:25:56 +0800

    1,添加注释，优化代码规范 2,添加后置网关过滤器,跨域请求。

---
+++ panoramic-monitor-gateway-zuul/src/main/java/com/cloud/filter/post/MyCorsFilter.java
@@ -0,0 +1,44 @@
+package com.cloud.filter.post;
+
+import com.netflix.zuul.ZuulFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author summer
+ * 后置过滤器
+ */
+@Component("corsFilter")
+public class MyCorsFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return "post";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        return bean;
+    }
+}
